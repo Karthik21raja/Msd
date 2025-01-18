f0d41a68fa65d00b306872e53f8ca12336c69d0d
Karthik21raja <karthikyuvaraj21@gmail.com>
2025-01-18 09:45:42 +0530

    Add files via upload

---
+++ jpaproj/src/main/java/com/example/jpa/JpaprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaprojApplication.class, args);
+	}
+
+}
