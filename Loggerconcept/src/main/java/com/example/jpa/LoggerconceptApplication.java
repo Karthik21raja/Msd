5fc9c3aa8089609e717d5073b94a53e2aa7f4c95
Karthik21raja <karthikyuvaraj21@gmail.com>
2025-01-20 11:10:38 +0530

    Add files via upload

---
+++ Loggerconcept/src/main/java/com/example/jpa/LoggerconceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerconceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerconceptApplication.class, args);
+	}
+
+}
